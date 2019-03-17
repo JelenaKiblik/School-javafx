71717da7716958ef013b5cb43be47020dc015448
aremme <aremme@ttu.ee>
2019-03-17 23:39:11 +0200

    music

---
+++ src/game/music/Music.java
@@ -0,0 +1,47 @@
+package game.music;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.nio.file.Paths;
+
+public class Music {
+
+    private MediaPlayer mediaPlayer;
+    private boolean musicOn = true;
+    private ImageView musicOnOrOff = new ImageView("resources/musicOn.png");
+    private Image musicOnImage = new Image("resources/musicOn.png");
+    private Image musicOffImage = new Image("resources/musicOff.png");
+
+
+    public ImageView musicImage() {
+        musicOnOrOff.setX(500);
+        musicOnOrOff.setY(20);
+        musicOnOrOff.setFitHeight(50);
+        musicOnOrOff.setFitWidth(50);
+        return musicOnOrOff;
+    }
+
+    public void musicStartsOrStops() {
+        String path = "src/resources/music.mp3";
+        Media media = new Media( Paths.get(path).toUri().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();
+        musicOnOrOff.setOnMouseClicked(event -> {
+            if (musicOn) {
+                mediaPlayer.stop();
+                musicOn = false;
+                musicOnOrOff.setImage(null);
+                musicOnOrOff.setImage(musicOffImage);
+            } else {
+                mediaPlayer.play();
+                musicOn = true;
+                musicOnOrOff.setImage(null);
+                musicOnOrOff.setImage(musicOnImage);
+            }
+        });
+    }
+}
