82e4e5205b4603c3bd95fc1f3844b06d5c1a034b
jelen <jekibl@ttu.ee>
2019-04-19 17:01:20 +0300

    javadoc for Villains class

--- src/game/elements/Villains.java
+++ src/game/elements/Villains.java
@@ -10,17 +10,50 @@ import java.util.Random;
 
 public class Villains {
 
+    /**
+     * Regulator for the speed.
+     */
     private static final int VILLAINS_SPEED_REGULATOR = 600;
+    /**
+     * Image for the villains.
+     */
     private Image villainImage = new Image(getClass().getResourceAsStream("/resources/villains.png"));
+    /**
+     * Arraylist that contains villain node.
+     */
     private List<Node> villains = new ArrayList<>();
+    /**
+     * Speed for the villain moving.
+     */
     private static final int VILLAINS_SPEED = -5;
+    /**
+     * Regulator for the villain speed.
+     */
     private int villainsSpeedRegulator = VILLAINS_SPEED_REGULATOR;
+    /**
+     * Villains count.
+     */
     private int villainsCount = villainsSpeedRegulator - 1;
+    /**
+     * Enemy node.
+     */
     private Node enemy = null;
+    /**
+     * Random generator for villains location.
+     */
     private Random random = new Random();
-    private static final int VILLIAN_MAX_Y_COORDINATE = 340;
+    /**
+     * Maximum y coordinate for the villain.
+     */
+    private static final int VILLIAN_MAX_Y_COORDINATE = 350;
+    /**
+     * Minimum y coordinate for the villain.
+     */
     private static final int VILLIAN_MIN_Y_COORDINATE = 200;
 
+    /**
+     * New villain located.
+     */
     public void newEnemy() {
         for (int i = 0; i < villains.size(); i++) {
             if (villains.get(i).getLayoutX() > -villains.get(i).getBoundsInLocal().getWidth()) {
@@ -31,6 +64,11 @@ public class Villains {
         }
     }
 
+    /**
+     * New villain appears on the screen.
+     *
+     * @param root root for the firstScene.
+     */
     public void newVillainAppears(Group root) {
         villainsCount++;
         if (villainsCount % villainsSpeedRegulator == 0) {
@@ -46,8 +84,12 @@ public class Villains {
         }
     }
 
+    /**
+     * Get enemy.
+     *
+     * @return enemy.
+     */
     public Node getEnemy() {
         return enemy;
     }
 }
-
