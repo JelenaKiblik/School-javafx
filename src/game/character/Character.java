2ed815d914c4652d0a6ca098af643a4e203e96bb
jelen <jekibl@ttu.ee>
2019-04-19 14:30:17 +0300

    javadoc for Character class

--- src/game/character/Character.java
+++ src/game/character/Character.java
@@ -8,26 +8,58 @@ import javafx.scene.text.Font;
 
 public class Character {
 
+    /**
+     * Label for the heading.
+     */
     private Label heading = new Label();
-
+    /**
+     * Image for the.
+     */
     private Image imageSpiderman = new Image(getClass().getResourceAsStream("/resources/spiderman_character.png"));
+    /**
+     * Image for the.
+     */
     private Image imageBatman = new Image(getClass().getResourceAsStream("/resources/batman_character.png"));
+    /**
+     * Image for the.
+     */
     private Image imageSuperman = new Image(getClass().getResourceAsStream("/resources/superman_character.png"));
-
+    /**
+     * Image for the.
+     */
     private Image imageSpidermanChosen = new Image(getClass().getResourceAsStream("/resources/spiderman_chosen.png"));
+    /**
+     * Image for the.
+     */
     private Image imageBatmanChosen = new Image(getClass().getResourceAsStream("/resources/batman_chosen.png"));
+    /**
+     * Image for the.
+     */
     private Image imageSupermanChosen = new Image(getClass().getResourceAsStream("/resources/superman_chosen.png"));
-
+    /**
+     * Image for the.
+     */
     private ImageView imageSpidermanPicked = new ImageView(imageSpiderman);
+    /**
+     * Image for the.
+     */
     private ImageView imageBatmanPicked = new ImageView(imageBatman);
+    /**
+     * Image for the.
+     */
     private ImageView imageSupermanPicked = new ImageView(imageSuperman);
-
-
+    /**
+     * Image for the.
+     */
     private ImageView chosenCharacter = new ImageView();
-
+    /**
+     * HBox for character selection.
+     */
     private HBox characterOptions = new HBox(imageSpidermanPicked, imageSupermanPicked, imageBatmanPicked);
 
-
+    /**
+     * @return heading.
+     */
     public Label showHeading() {
         heading.setText("Vali karakter");
         heading.setTranslateX(270);
@@ -37,42 +69,75 @@ public class Character {
         return heading;
     }
 
+    /**
+     * @return spider-man image.
+     */
     public Image getSpidermanCharacterChosen() { return imageSpidermanChosen; }
 
+    /**
+     * @return batman image.
+     */
     public Image getBatmanCharacterChosen() {
         return imageBatmanChosen;
     }
 
+    /**
+     * @return superman image.
+     */
     public Image getSupermanCharacterChosen() {
         return imageSupermanChosen;
     }
 
+    /**
+     * @return chosen character.
+     */
     public ImageView getChosenCharacter() {
         return chosenCharacter;
     }
 
+    /**
+     * @return  spider-man image.
+     */
     public Image getSpiderman() {
         return imageSpiderman;
     }
 
+    /**
+     * @return superman image.
+     */
     public Image getSuperman() {
         return imageSuperman;
     }
 
+    /**
+     * @return batman image.
+     */
     public Image getBatman() {
         return imageBatman;
     }
 
+    /**
+     * @return spider-man image view.
+     */
     public ImageView getSpidermanCharacterPicked() {
         return imageSpidermanPicked;
     }
 
+    /**
+     * @return batman image view.
+     */
     public ImageView getBatmanCharacterPicked() { return imageBatmanPicked; }
 
+    /**
+     * @return superman image view.
+     */
     public ImageView getSupermanCharacterPicked() {
         return imageSupermanPicked;
     }
 
+    /**
+     * @return characterOptions hbox.
+     */
     public HBox getCharacterOptions() {
         return characterOptions;
     }
