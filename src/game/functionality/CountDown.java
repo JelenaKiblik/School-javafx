ce00298d135b0f8e79b4d646278e9911a1ccf376
jelen <jekibl@ttu.ee>
2019-04-22 10:54:19 +0300

    javadoc for CountDown class

--- src/game/functionality/CountDown.java
+++ src/game/functionality/CountDown.java
@@ -8,16 +8,48 @@ import javafx.util.Duration;
 
 public class CountDown {
 
+    /**
+     * Label for the count down numbers.
+     */
     private Label countDown = new Label();
+    /**
+     * Count down starting point.
+     */
     private int countNr = 2;
+    /**
+     * Checks if count down is finished.
+     */
     private boolean finished;
+    /**
+     * Countdown label y coordinates.
+     */
     private static final int COUNTDOWN_Y_COORDINATES = 300;
+    /**
+     * Countdown label x coordinates.
+     */
     private static final int COUNTDOWN_X_COORDINATES = 300;
-    private static final int COUNTDOWN_TWO_DURATION = 1000;
-    private static final int COUNTDOWN_ONE_DURATION = 2000;
+    /**
+     * Duration dor countdown zero.
+     */
     private static final int COUNTDOWN_ZERO_DURATION = 3000;
+    /**
+     * Duration for countdown one.
+     */
+    private static final int COUNTDOWN_ONE_DURATION = 2000;
+    /**
+     * Duration for countdown two.
+     */
+    private static final int COUNTDOWN_TWO_DURATION = 1000;
+    /**
+     * Countdown decrease.
+     */
     private static final int COUNTDOWN_DECREASE = 2;
 
+    /**
+     * Count down for the game to start.
+     *
+     * @param root root for the firstScene.
+     */
     public void countdown(Group root) {
         finished = false;
         Timeline countDownThreeTwo = new Timeline();
@@ -27,6 +59,7 @@ public class CountDown {
         countDown.setText(String.valueOf(countNr));
         countDown.getStyleClass().add("labelCountDown");
         root.getChildren().addAll(countDown);
+
         KeyFrame two = new KeyFrame(Duration.millis(COUNTDOWN_TWO_DURATION), event -> {
             countDown.setText(String.valueOf(countNr - 1));
             countDown.getStyleClass().add("labelCountDown");
@@ -37,6 +70,7 @@ public class CountDown {
         });
         KeyFrame zero = new KeyFrame(Duration.millis(COUNTDOWN_ZERO_DURATION), event ->
                 countDown.setText(""));
+
         countDownOne.getKeyFrames().addAll(two);
         countDownOne.setCycleCount(1);
         countDownOne.play();
@@ -46,10 +80,18 @@ public class CountDown {
         countDownThreeTwo.setOnFinished(event -> setFinished());
     }
 
+    /**
+     * Set count down finished.
+     */
     private void setFinished() {
         this.finished = true;
     }
 
+    /**
+     * Checks if count down is finished.
+     *
+     * @return boolean.
+     */
     public boolean isFinished() {
         return finished;
     }
