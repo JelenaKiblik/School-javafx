c41a982c7699627106bd285b66ba15835daf23c1
jelen <jekibl@ttu.ee>
2019-04-22 10:12:07 +0300

    javadoc for Collision class

--- src/game/functionality/Collision.java
+++ src/game/functionality/Collision.java
@@ -1,6 +1,5 @@
 package game.functionality;
 
-
 import game.elements.Lives;
 import game.scoreboard.Scoreboard;
 import javafx.geometry.Bounds;
@@ -10,12 +9,26 @@ import javafx.scene.image.ImageView;
 
 public class Collision {
 
+    /**
+     * Points for shooting.
+     */
     private static final int POINTS_FOR_SHOOTING = 2;
 
-    public void collide(ImageView chosenCharacter, Boolean shooting, Node enemy, Scoreboard scoreboard, Node bulletFire, Image fireImage,
-                        Lives lives) {
+    /**
+     * Checks collision when shooting or when character touches pipes or villains.
+     *
+     * @param chosenCharacter       character chosen for the game.
+     * @param shooting              checks if shooting is happening.
+     * @param enemy                 villain.
+     * @param bulletFire            fire used for shooting.
+     * @param fireImage             image for the fire.
+     * @param lives                 character lives.
+     */
+    public void collide(ImageView chosenCharacter, Boolean shooting, Node enemy, Scoreboard scoreboard, Node bulletFire,
+                        Image fireImage, Lives lives) {
         Bounds objCharacter = chosenCharacter.localToScene(chosenCharacter.getBoundsInLocal());
         Bounds objVillain = enemy.localToScene(enemy.getBoundsInLocal());
+
         if (shooting) {
             Bounds objFire = bulletFire.localToScene(bulletFire.getBoundsInLocal());
             if (objFire.intersects(objVillain) && enemy.isVisible()) {
