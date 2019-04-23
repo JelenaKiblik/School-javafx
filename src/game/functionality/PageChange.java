55686851e5feff63c2758225903a1cb61ef68f6f
jelen <jekibl@ttu.ee>
2019-04-23 15:55:55 +0300

    javadoc for Instructions, clean code

--- src/game/functionality/PageChange.java
+++ src/game/functionality/PageChange.java
@@ -1,5 +1,8 @@
 package game.functionality;
 
+/**
+ * Enum for changing page.
+ */
 public enum PageChange {
     INFO, GAME, GAMEOVER, HEROES, SCORE, HOMEPAGE, SETTINGS
 }
--- src/game/instructions/Instructions.java
+++ src/game/instructions/Instructions.java
@@ -4,38 +4,46 @@ import javafx.scene.control.Label;
 import java.io.IOException;
 import javafx.scene.paint.Color;
 
-
 public class Instructions {
+
+    /**
+     * Label for the instructions.
+     */
     private Label info = new Label();
+    /**
+     * Label for the heading of instructions.
+     */
     private Label heading = new Label();
+    /**
+     * Heading x coordinates.
+     */
     private static final int HEADING_X_COORDINATES = 300;
+    /**
+     * Heading y coordinates.
+     */
     private static final int HEADING_Y_COORDINATES = 150;
 
-    public Label showInfo() throws IOException {
+    /**
+     * Show the instructions.
+     *
+     * @return label with the instructions.
+     */
+    public Label showInfo() {
         info.setText("- Mängu alustades on vaja sisestada nimi ja valida oma hero\n" +
                 "- Hero liigutamiseks on vaja vajutada ekraanile\n" +
                 "- Mäng läbi kui hero puudutab takistus\n" +
                 "- Mäng läbi kui hero kukkub\n");
         info.setTranslateX(80);
         info.setTranslateY(250);
-        //info.setFont(new Font("Arial", 25));
-        //info.setTextFill(Color.WHITE);
         info.getStyleClass().add("instructionsBorder");
         return info;
-
-//        File file = new File("game/Instructions.txt");
-//        FileInputStream fis = new FileInputStream(file);
-//        byte[] data = new byte[(int) file.length()];
-//        fis.read(data);
-//        fis.close();
-//        String str = new String(data, "UTF-8");
-//        info.setText(str);
-//        info.setTranslateX(INFO_X_COORDINATES);
-//        info.setTranslateY(INFO_y_COORDINATES);
-//        info.getStyleClass().add("instructions");
-//        return info;
     }
 
+    /**
+     * Show heading of the instructions.
+     *
+     * @return label with the heading.
+     */
     public Label heading() {
         heading.setText("Juhend");
         heading.setTranslateX(HEADING_X_COORDINATES);
